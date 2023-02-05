30f7de079f4b3b1fc62f8fd3a289983c8c32109a
NatashaPet13 <114287220+NatashaPet13@users.noreply.github.com>
2023-02-05 03:29:36 +0300

    Задача 1

---
+++ less6.1.py
@@ -0,0 +1,7 @@
+from functools import reduce
+dot_1 = list(map(int, input('Координаты первой точки A: ').split())) 
+dot_2 = list(map(int, input('Координаты второй точки B: ').split()))
+def dot_range(dot_1, dot_2):
+     rng = reduce(lambda x, y: (x + y)**(1/2), (map(lambda dot: (dot[1] - dot[0])**2, zip(dot_1, dot_2))))
+     return round(rng, 2)
+print(dot_range(dot_1, dot_2))
